763aa73431dfdd0b90ca046b4f5d585565a6ce48
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2018-09-22 06:06:33 +0000

    change to fog

--- config/initializers/carrieawave.rb
+++ config/initializers/carrieawave.rb
@@ -1,40 +1,40 @@
-# #fog setting
-# if Rails.env == 'production'
-#   CarrierWave.configure do |config|
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#       region: 'ap-northeast-1'
-#     }
-#     config.fog_directory = ENV['AWS_BUCKET']
-#     config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/' + ENV['AWS_BUCKET']
-#   end
-# end
-
-## ftp setting
-# CarrierWave.configure do |config|
-  #   config.ftp_host = ENV['FTP_HOST']
-  #   config.ftp_port = 21
-  #   config.ftp_user = ENV['FTP_USER']
-  #   config.ftp_passwd = ENV['FTP_PASS']
-  #   config.ftp_folder = ENV['FTP_PATH']
-  #   config.ftp_url = ENV['FTP_URL']
-  #   config.ftp_passive = false
-  #   config.ftp_tls = false
-  # end
-
-# sftp setting
+#fog setting
 if Rails.env == 'production'
   CarrierWave.configure do |config|
-    config.sftp_host = ENV['FTP_HOST']
-    config.sftp_user = ENV['FTP_USER']
-    config.sftp_folder = ENV['FTP_PATH']
-    config.sftp_url = ENV['FTP_URL']
-    config.sftp_options = {
-      :password => ENV['FTP_PASS'],
-      :port => 22
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1'
     }
+    config.fog_directory = ENV['AWS_BUCKET']
+    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/' + ENV['AWS_BUCKET']
   end
 end
+
+# # ftp setting
+# CarrierWave.configure do |config|
+#   config.ftp_host = ENV['FTP_HOST']
+#   config.ftp_port = 21
+#   config.ftp_user = ENV['FTP_USER']
+#   config.ftp_passwd = ENV['FTP_PASS']
+#   config.ftp_folder = ENV['FTP_PATH']
+#   config.ftp_url = ENV['FTP_URL']
+#   config.ftp_passive = false
+#   config.ftp_tls = false
+# end
+
+# # sftp setting
+# if Rails.env == 'production'
+#   CarrierWave.configure do |config|
+#     config.sftp_host = ENV['FTP_HOST']
+#     config.sftp_user = ENV['FTP_USER']
+#     config.sftp_folder = ENV['FTP_PATH']
+#     config.sftp_url = ENV['FTP_URL']
+#     config.sftp_options = {
+#       :password => ENV['FTP_PASS'],
+#       :port => 22
+#     }
+#   end
+# end
