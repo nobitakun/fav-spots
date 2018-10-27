0207e88e96d9e74be0f91548e18f3aca4395852a
Kuroiwa Taku <kuroiwa.taku@gmail.com>
2018-10-27 17:28:57 +0000

    change sftp

--- config/initializers/carrieawave.rb
+++ config/initializers/carrieawave.rb
@@ -1,17 +1,17 @@
 #fog setting
-if Rails.env == 'production'
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-    config.fog_directory = ENV['AWS_BUCKET']
-    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/' + ENV['AWS_BUCKET']
-  end
-end
+# if Rails.env == 'production'
+#   CarrierWave.configure do |config|
+#     config.fog_provider = 'fog/aws'
+#     config.fog_credentials = {
+#       provider: 'AWS',
+#       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#       region: 'ap-northeast-1'
+#     }
+#     config.fog_directory = ENV['AWS_BUCKET']
+#     config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/' + ENV['AWS_BUCKET']
+#   end
+# end
 
 # # ftp setting
 # CarrierWave.configure do |config|
@@ -26,18 +26,18 @@ end
 # end
 
 # # sftp setting
-# if Rails.env == 'production'
-#   CarrierWave.configure do |config|
-#     config.sftp_host = ENV['FTP_HOST']
-#     config.sftp_user = ENV['FTP_USER']
-#     config.sftp_folder = ENV['FTP_PATH']
-#     config.sftp_url = ENV['FTP_URL']
-#     config.sftp_options = {
-#       :password => ENV['FTP_PASS'],
-#       :port => 22
-#     }
-#   end
-# end
+if Rails.env == 'production'
+  CarrierWave.configure do |config|
+    config.sftp_host = ENV['FTP_HOST']
+    config.sftp_user = ENV['FTP_USER']
+    config.sftp_folder = ENV['FTP_PATH']
+    config.sftp_url = ENV['FTP_URL']
+    config.sftp_options = {
+      :password => ENV['FTP_PASS'],
+      :port => 22
+    }
+  end
+end
 
 module CarrierWave
   module MiniMagick
