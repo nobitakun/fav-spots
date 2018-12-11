42ba8aeb86f88d2eaf9cb687ee2623857a923bfd
nobitakun <dokodemodoor2013@gmail.com>
2018-12-11 15:27:44 +0000

    add top article list

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,16 +4,27 @@ class StaticPagesController < ApplicationController
     @reviews = Review.limit(4).order('created_at DESC')
     @infos = Info.limit(4).order('created_at DESC')
     @prefs = JpPrefecture::Prefecture.all
+    @wp_path = ENV['WP_PATH']
     
-    conn = Faraday.new(url: ENV['WP_PATH']) do |faraday|
-    faraday.request  :url_encoded
-    faraday.response :logger
-    faraday.adapter  Faraday.default_adapter
+    conn = Faraday.new(url: @wp_path) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
+    end
+    
+    conn_experience = Faraday.new(url: @wp_path) do |faraday|
+      faraday.request  :url_encoded
+      faraday.response :logger
+      faraday.adapter  Faraday.default_adapter
     end
 
-    response = conn.get '/wp-json/wp/v2/posts?_embed'  
-
-    @parsed_articles = JSON.parse(response.body) 
+    response = conn.get '/wp-json/wp/v2/columns?_embed'
+    
+    @parsed_columns = JSON.parse(response.body) 
+    
+    response = conn_experience.get '/wp-json/wp/v2/experiences?_embed' 
+    
+    @parsed_experiences = JSON.parse(response.body) 
   end
 
   def dbmap
